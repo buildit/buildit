c1e251170a65c0a7e2f09fdc29040d52607157b4
Panos Voudouris <panosvoudouris@gmail.com>
2017-03-18 00:41:15 +0000

    Cleanup gulp and npm scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 const gulp = require('gulp');
 const clean = require('gulp-clean');
 const gulpCopy = require('gulp-copy');
+
 const src = 'src';
 const target = 'dist';
 
 gulp.task('clean', () => {
   return gulp.src(target, {read: false})
-          .pipe(clean());
+    .pipe(clean());
 });
 
-gulp.task('html', () => {
+// Copy everything except CSS
+gulp.task('copy-files', () => {
   return gulp.src([`${src}/**`, `!${src}/**/*.css`])
     .pipe(gulp.dest(target));
 });
@@ -19,5 +21,5 @@ gulp.task('css', () => {
     .pipe(gulp.dest(target));
 });
 
+gulp.task('build', ['copy-files', 'css']);
 gulp.task('default', ['clean', 'build']);
-gulp.task('build', ['html', 'css']);
