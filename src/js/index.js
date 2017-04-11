881ec86ecfc09b1f2e85e85dd17762887a8a2495
Tom Robinson <tom.robinson@wipro.com>
2017-04-11 16:03:51 +0100

    Re-coded menu from scratch

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,20 @@
 /**
  * Created by jonlazarini on 21/03/17.
  */
-$(document).ready(function() {
+$(document).ready(function () {
 
-  $('.masthead')
+  $('.navigation')
     .visibility({
       once: false,
-      onBottomPassed: function() {
-        $('.fixed.menu').transition('fade in');
+      onBottomPassed: function () {
+        $('.secondary-navigation').transition('fade in');
       },
-      onBottomPassedReverse: function() {
-        $('.fixed.menu').transition('fade out');
+      onBottomPassedReverse: function () {
+        $('.secondary-navigation').transition('fade out');
       }
     });
 
-  // nav on careers, make
-  $('.nomasta')
-    .visibility({
-      once: false,
-      onBottomPassed: function() {
-        $('.fixed.menu').transition('fade in');
-      },
-      onBottomPassedReverse: function() {
-        $('.fixed.menu').transition('fade out');
-      }
-    })
-  ;
+  $('.ui.sidebar').sidebar('attach events', '.toc.item');
 
   // lazy load images
   $('.image').visibility({
@@ -37,51 +26,19 @@ $(document).ready(function() {
   /**
    * Beginning side links animation
    * */
-      var $links;
-      var handler
-        ;
-      $links = $('.item.nav-link')
+  var $links;
+  var handler;
+  $links = $('.item.nav-link');
 
-      handler = {
-        activate: function() {
-          // console.log('this in handler', this)
-          $(this)
-            .addClass('active')
-            .siblings()
-            .removeClass('active')
-        }
-      }
-      // init
-      $links.on('click', handler.activate)
+  handler = {
+    activate: function () {
+      $(this)
+        .addClass('active')
+        .siblings()
+        .removeClass('active')
+    }
+  }
+
+  $links.on('click', handler.activate)
   /** end side links animation **/
-
-  $('button').on('click', function() {
-    $('.sidebar')
-      /*.sidebar({
-        dimPage: true,
-        scrollLock: true,
-        transition: {
-          computer: 'top'
-        }
-      })*/
-      .sidebar({
-        dimPage: true,
-        scrollLock: true,
-        closable: true,
-        transition: 'auto',
-        easing: 'easeInOutQuint',
-        // debug
-        debug: true,
-        performance: true,
-        silent: false,
-
-        duration: 2000,
-        onVisible: function() {console.log('opened')}
-      })
-      .sidebar('toggle')
-    ;
-  })
-
-  console.log('end scripts')
-
-})
+});
