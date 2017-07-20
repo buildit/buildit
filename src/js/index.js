c51395eae279f040e204934a411fc0531909ed6c
byronwong <byron.wong@wipro.com>
2017-07-20 11:12:54 +0100

    added links to job pages and 2 column layout

--- src/js/index.js
+++ src/js/index.js
@@ -23,12 +23,13 @@ $(document).ready(function () {
 
   $('.image').visibility('refresh');
 
+  // Template for jobs data on careers page
   function tpl(jobData) {
-    var template = '<li class="opening-job job column wide-1of2 medium-1of2">';
-    template += '<a href="https://jobs.smartrecruiters.com/" class="link--block details">';
+    var template = '<li class="opening-job job column eight wide">';
+    template += '<a href="https://jobs.smartrecruiters.com/ni/WiproDigital/' + jobData.uuid + '" class="link--block details">';
     template += '<h3 class="details-title job-title link--block-target">' + jobData.name + '</h3>';
     template += '<ul class="details-desc desc-items list--dotted">';
-    template += '<li class="desc-item">' + jobData.location.city + ', ' + jobData.region + '</li>';
+    template += '<li class="desc-item">' + jobData.location.city + ', ' + jobData.customField[1].valueLabel + '</li>';
     template += '<li class="desc-item">' + jobData.typeOfEmployment.label + '</li>';
     template += '</ul>'
     template += '</a>';
@@ -36,41 +37,53 @@ $(document).ready(function () {
     return template;
   }
 
-  function matchCity(city) {
-    return function (data) {
-      return data.location.city.toLowerCase() === city;
+  // const matchCity = city => data => data.location.city === city;
+  // Filter by city
+  // function matchCity(city) {
+  //   return function (data) {
+  //     return data.location.city.toLowerCase() === city;
+  //   }
+  // }
+
+  // Sort By Country
+  function sortCountry(a, b) {
+    var x = a.location.country;
+    var y = b.location.country;
+
+    if (x > y) {
+      return 1;
     }
+    if (x < y) {
+      return -1;
+    }
+    return 0;
   }
 
-  // const matchCity = city => data => data.location.city === city;
-
-  if ($('ul.opening-jobs.grid--gutter')) {
-
+  if ($('ul#jobs-board').length) {
     (function () {
 
       var url = "https://api.smartrecruiters.com/v1/companies/WiproDigital/postings?";
-      var limitRequest = 'offset=10&limit=10';
-      var customField = 'custom_field.5880c55be4b0cfde272956ad=83455af9-c888-4221-9312-4750b5a09bf5';
+      // get jobs that are Buildit not WD
+      var getBrand = 'custom_field.5880c55be4b0cfde272956ad=83455af9-c888-4221-9312-4750b5a09bf5';
 
-      fetch(url + customField).then(function (response) {
+      fetch(url + getBrand).then(function (response) {
         if (!response.ok) {
           throw Error(response.statusText);
         }
         return response.json();
       })
         .then(function (response) {
-          // console.log(response.content);
-          // console.log(response.content.length);
           var data = response.content;
+
+          // console.log(data);
           var wrapper = $('ul.opening-jobs');
 
-          // .filter(matchCity('toronto'))
+          // .filter(matchCity('uk'))
           wrapper.append(
-            data
-              .map(tpl)
-              .join('')
+            data.sort(sortCountry)
+                .map(tpl)
+                .join('')
           );
-
         })
         .catch(function (error) {
           console.log(error);
