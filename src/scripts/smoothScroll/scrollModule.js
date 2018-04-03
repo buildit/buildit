490c2743488a702e315990e8ed74b9d28191bc87
Ana Escontrela <anaescontrela@Anas-MacBook-Pro.local>
2018-04-03 15:05:15 +0100

    - Remove ES6 Class syntax

--- src/scripts/smoothScroll/scrollModule.js
+++ src/scripts/smoothScroll/scrollModule.js
@@ -1,75 +1,74 @@
 import easingOptions from "./easingOptions.js";
 
-const defaultSpeed = 2500;
+const defaultOptions = {
+  duration: 2500,
+  animation: easingOptions.easeOutCubic,
+};
 
-class Scroll {
-  constructor(opts) {
-    this.options = {
-      class: opts.class,
-      animation: opts.animation,
-      duration: opts.duration,
-    };
-    this._time = {};
-    this._position = {};
-  }
-
-  init() {
-    this._setDefaults();
-    document.querySelectorAll(this.options.class).forEach(element => {
-      if (
-        element.href.indexOf(window.location.origin) !== -1 &&
-        element.href.indexOf("#") !== -1
-      ) {
-        this.onScroll(element);
-      }
-    });
-  }
-
-  onScroll(element) {
-    element.addEventListener("click", () =>
-      window.requestAnimationFrame(() => {
-        this._position.start = window.screenY;
-        this._position.end = window.scrollY;
-        this._position.current = this._position.start;
-        this.updateState();
-      })
-    );
-  }
-
-  updateState() {
-    const now = new Date();
-
-    this._time.start === null
-      ? ((this._time.start = now), (this._time.now = now))
-      : (this._time.now = now);
-
-    this._position.current < this._position.end
-      ? (this.onChange(),
-        window.requestAnimationFrame(() => this.updateState()))
-      : this._setDefaults(),
-      window.cancelAnimationFrame(this.updateState);
-  }
-
-  // See https://github.com/cferdinandi/smooth-scroll/blob/master/dist/js/smooth-scroll.js
-  onChange() {
-    const elapsedTime = this._time.now - this._time.start;
-    let progress = elapsedTime / this.options.duration;
-    progress = progress >= 1 ? 1 : progress;
-
-    this._position.current =
-      this._position.start +
-      this._position.end * this.options.animation(progress);
-    window.scrollTo(0, this._position.current);
-  }
-
-  _setDefaults() {
-    this.options.animation =
-      this.options.animation || easingOptions.easeOutCubic;
-    this.options.duration = this.options.duration || 2500;
-
-    this._time = { start: null, now: null };
-    this._position = { start: 0, end: null, current: null };
-  }
+function Scroll(opts) {
+  this.options = {
+    class: opts.class,
+    animation: opts.animation,
+    duration: opts.duration,
+  };
+  this._time = {};
+  this._position = {};
 }
+
+Scroll.prototype.init = function() {
+  this._setDefaults();
+  document.querySelectorAll(this.options.class).forEach(element => {
+    if (
+      element.href.indexOf(window.location.origin) !== -1 &&
+      element.href.indexOf("#") !== -1
+    ) {
+      this.onScroll(element);
+    }
+  });
+};
+
+Scroll.prototype.onScroll = function(element) {
+  element.addEventListener("click", () =>
+    window.requestAnimationFrame(() => {
+      this._position.start = window.screenY;
+      this._position.end = window.scrollY;
+      this._position.current = this._position.start;
+      this.updateState();
+    })
+  );
+};
+
+Scroll.prototype.updateState = function() {
+  const now = new Date();
+
+  this._time.start === null
+    ? ((this._time.start = now), (this._time.now = now))
+    : (this._time.now = now);
+
+  this._position.current < this._position.end
+    ? (this.onChange(), window.requestAnimationFrame(() => this.updateState()))
+    : this._setDefaults(),
+    window.cancelAnimationFrame(this.updateState);
+};
+
+// See https://github.com/cferdinandi/smooth-scroll/blob/master/dist/js/smooth-scroll.js
+Scroll.prototype.onChange = function() {
+  const elapsedTime = this._time.now - this._time.start;
+  let progress = elapsedTime / this.options.duration;
+  progress = progress >= 1 ? 1 : progress;
+
+  this._position.current =
+    this._position.start +
+    this._position.end * this.options.animation(progress);
+  window.scrollTo(0, this._position.current);
+};
+
+Scroll.prototype._setDefaults = function() {
+  this.options.animation = this.options.animation || defaultOptions.animation;
+  this.options.duration = this.options.duration || defaultOptions.duration;
+
+  this._time = { start: null, now: null };
+  this._position = { start: 0, end: null, current: null };
+};
 
 export default Scroll;
