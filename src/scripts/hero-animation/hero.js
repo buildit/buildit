d724eb49e4d54823ba37a4fa0cdc26ad1f05e943
James McMillan <jamesmcmillan@Jamess-MacBook-Pro.local>
2018-04-04 10:44:03 +0100

    added all scripts and working test markup

--- src/scripts/hero-animation/hero.js
+++ src/scripts/hero-animation/hero.js
@@ -12,10 +12,10 @@
       height = window.innerHeight;
       target = {x: width/2, y: height/2};
 
-      largeHeader = document.getElementById('large-header');
+      largeHeader = document.getElementById('js-large-header');
       largeHeader.style.height = height+'px';
 
-      canvas = document.getElementById('demo-canvas');
+      canvas = document.getElementById('js-demo-canvas');
       canvas.width = width;
       canvas.height = height;
       ctx = canvas.getContext('2d');
