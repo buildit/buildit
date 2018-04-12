ff11faf77fa436875e55164caaf3590a8ad53ee9
Ana Escontrela <ana.escontrela@wipro.com>
2018-04-12 12:07:50 +0100

    WEB-161 Soft scroll fix -  remove reference to scrolldestination dataset at ._getElementPosition - remove scrollModule notation from main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,8 @@
-/**
- * @param { String || Array<string> } Class
- * @param { string } Animation
- * @param { number } Duration
- * @returns { Object } Handles the scroll to an anchor on the same page
- */
-
 import Scroll from "./smoothScroll/scrollModule.js";
-const scroll = new Scroll({ class: ".grav-c-cta" });
-scroll.init();
-
 import ScrollReveal from "./scrollReveal/scrollRevealModule.js";
+
+const scroll = new Scroll({ class: ".grav-c-cta" });
 const scrollReveal = new ScrollReveal();
+
+scroll.init();
 scrollReveal.init();
