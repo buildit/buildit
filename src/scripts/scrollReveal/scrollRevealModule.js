681608bff625fdddc8a398e7c0def7774757ed82
Patrick Bower <bower.patrick@gmail.com>
2018-04-13 15:50:06 +0100

    Remove animation on mobile devices

--- src/scripts/scrollReveal/scrollRevealModule.js
+++ src/scripts/scrollReveal/scrollRevealModule.js
@@ -4,7 +4,8 @@ const defaults = {
   revealElement: ".js-reveal-element",
   originParams: ["top", "right", "bottom", "left"],
   randomDurationPeriod: [500, 1500],
-  randomDistanceLength: [100, 500]
+  randomDistanceLength: [100, 500],
+  mobile: false
 };
 
 class ScrollRevealModule {
@@ -14,29 +15,34 @@ class ScrollRevealModule {
 
   init() {
     document.querySelectorAll(defaults.revealElement).forEach(ele => {
-      let randomOptions = this.generateRandomOptions(ele.dataset.exclude);
-      scrollreveal.reveal(ele, randomOptions);
+      let options = this.generateOptions(ele.dataset.exclude);
+      scrollreveal.reveal(ele, options);
     });
   }
 
-  generateRandomOptions(excludeOrigin) {
-    let randomOptions = {};
+  generateOptions(excludeOrigin) {
+    let options = {};
 
-    randomOptions.duration = utils.getRandomIntInclusive(
+    // duration - animation time period
+    options.duration = utils.getRandomIntInclusive(
       defaults.randomDurationPeriod
     );
 
+    // origin - starting point for of animation
     let originOptions = defaults.originParams.filter(
       item => item !== excludeOrigin
     );
-    randomOptions.origin =
-      originOptions[utils.getRandomInt(originOptions.length)];
+    options.origin = originOptions[utils.getRandomInt(originOptions.length)];
 
-    randomOptions.distance =
+    // distance - animation travel span
+    options.distance =
       Math.floor(utils.getRandomIntInclusive(defaults.randomDistanceLength)) +
       "px";
 
-    return randomOptions;
+    // mobile - include animations on mobile
+    options.mobile = defaults.mobile;
+
+    return options;
   }
 }
 
