8036fa0bff232aa6855bc8624ae064ee01a7b17f
Dan Bull <daniel.bull@wipro.com>
2018-04-16 14:42:50 +0100

    Tidy up

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -7,7 +7,3 @@ export function getRandomIntInclusive(values) {
   max = Math.floor(max);
   return Math.random() * (max - min + 1) + min;
 }
-
-export function getDistance(p1, p2) {
-  return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
-}
