efd4a1c27df45097d8ab97404d6b25efd34ac4cc
Ana Escontrela <ana.escontrela@wipro.com>
2018-04-16 13:49:15 +0100

    update tests

--- cypress/integration/careers_spec.js
+++ cypress/integration/careers_spec.js
@@ -11,13 +11,6 @@ describe("Careers page", () => {
     cy.title().should("include", route);
   });
 
-  it("should indicates current route as active on the navigation menu", () => {
-    cy
-      .get(".grav-c-block-link")
-      .find(`a[title='${route}']`)
-      .should("not.has.attr", "href");
-  });
-
   it("renders an anchor that uses the smoothScroll feature", () => {
     cy.smoothScroll(`#${anchor}`);
   });
