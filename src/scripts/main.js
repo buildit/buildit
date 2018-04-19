68de74644040b7805fdbdc5fc561474f48684a0e
danbull <daniel.bull@me.com>
2018-04-19 11:35:47 +0100

    Web 141 (#112)
    * WIP refactoring hero animation
    
    * Changed gsap imports
    
    * Refactoring animation functions
    
    * Refactoring without using class
    
    * Refactoring and added mouse out event
    
    * Improving setTimeout function
    
    * Tidy up
    
    * Prettifying
    
    * fixing scroll bug on mobile
    
    * Adding defer to scripts
    
    * Making circles and lines less transparent
    
    * Renaming function
    
    * Changing console output type
    
    * Removing redundant js
    
    * Removing location redirect
    
    * Improving resize function
    
    * Tweaking debouncer duration
    
    * Fixing animation initialisation
    
    * Reverting back to CDN for now
    
    * Tidying up

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 import Scroll from "./smoothScroll/scrollModule.js";
 import ScrollReveal from "./scrollReveal/scrollRevealModule.js";
 import toogleSwitch from "./toogleSwitch/toogleSwitch.js";
+import HeroAnimation from './heroAnimation/hero.js';
 
 const scroll = new Scroll({ class: ".grav-c-cta" });
 const scrollReveal = new ScrollReveal();
+const heroAnimation = new HeroAnimation();
 
 scroll.init();
 scrollReveal.init();
