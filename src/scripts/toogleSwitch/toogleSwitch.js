06200ec65dd45aa87c9aa4223ad38e7c2caf8eb4
James Nash <james.nash@wipro.com>
2018-04-20 16:24:42 +0100

    Replaced NodeList.forEach() with IE11-friendly alternative. (#126)

--- src/scripts/toogleSwitch/toogleSwitch.js
+++ src/scripts/toogleSwitch/toogleSwitch.js
@@ -1,12 +1,16 @@
 const toogleSwitch = () => {
-  document
-    .querySelectorAll("[type=button][aria-pressed]")
-    .forEach(toggleButton => {
-      toggleButton.addEventListener("click", e => {
-        let pressed = toggleButton.getAttribute("aria-pressed") === "true";
-        toggleButton.setAttribute("aria-pressed", String(!pressed));
-      });
+  const toggleButtons = document.querySelectorAll(
+    "[type=button][aria-pressed]"
+  );
+
+  // IE-compatible way of iterating over the NodeList
+  // (See: https://developer.mozilla.org/en-US/docs/Web/API/NodeList)
+  Array.prototype.forEach.call(toggleButtons, toggleButton => {
+    toggleButton.addEventListener("click", e => {
+      let pressed = toggleButton.getAttribute("aria-pressed") === "true";
+      toggleButton.setAttribute("aria-pressed", String(!pressed));
     });
+  });
 };
 
 export default toogleSwitch;
