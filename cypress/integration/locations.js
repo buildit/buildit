153af246136a23f5e5c6a812dab535389c032349
ana <ana.escontrela@wipro.com>
2018-04-23 12:35:06 +0100

    add more tests and implements commands

---
+++ cypress/integration/locations.js
@@ -0,0 +1,26 @@
+describe("Locations page", () => {
+  let route;
+
+  beforeEach(() => {
+    route = "Locations";
+    cy.visit(`http://localhost:8080/locations`);
+  });
+
+  it("should render the page title", () => {
+    cy.title().should("include", "We are global");
+  });
+
+  it("should render current route as active navigation option", () => {
+    cy.isActiveRoute(".grav-c-block-link", route);
+  });
+
+  it("should render a map for each location that opens a new window", () => {
+    cy
+      .get(".grav-c-two-columns-block")
+      .find(".grav-c-location-card")
+      .each(value => {
+        expect(value.find("a").attr("href")).to.include("google");
+        expect(value.find("a").attr("target")).to.include("blank");
+      });
+  });
+});
