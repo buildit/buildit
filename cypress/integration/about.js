a1623c8243047af3598ebc848b1eb77b75706480
Ana Escontrela <ana.escontrela@wipro.com>
2018-04-25 10:49:47 +0100

    updates tests specs

---
+++ cypress/integration/about.js
@@ -0,0 +1,20 @@
+describe("About page", () => {
+  let route;
+
+  beforeEach(() => {
+    route = "About";
+    cy.visit(`/about`);
+  });
+
+  it("should render the page title", () => {
+    cy.title().should("include", route);
+  });
+
+  it("should render current route as active navigation option", () => {
+    cy.isActiveRoute(".grav-c-block-link", route);
+  });
+
+  it("renders a pullquote animation", () => {
+    cy.scrollReveal(".js-reveal-element");
+  });
+});
