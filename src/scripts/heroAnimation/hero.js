1d10159c5521927f097330a6166953f3225a6f0e
danbull <daniel.bull@me.com>
2018-04-30 15:50:06 +0100

    Web 195 (#134)
    * WIP refactoring
    
    * WIP refactoring of hero animation
    
    * Tidyup
    
    * Refactoring Circle function
    
    * Refactoring Circle function
    
    * Minor refactoring and initial test
    
    * added babel-jest
    
    - updated babelrc
    - added rollup config
    
    * Fixes Jest es module issue and avoid copying tests
    
    - Updates babel config to use env for jest tests
    - Updates modules copy script to skip .test.js files
    
    * Refactoring and adding Circle test
    
    * Minor change
    
    * Minor refactor
    
    * Adding drawLines tests
    
    * Adding getDistance test
    
    * added test for shiftPoint
    
    - refactored shiftPoint to allow mock injection
    
    * WIP hero tests
    
    * Refactoring
    
    * Refactoring mousemove function
    
    * Adding hover style animation for non touch devices
    
    * PR feedback

--- src/scripts/heroAnimation/hero.js
+++ src/scripts/heroAnimation/hero.js
@@ -1,122 +1,77 @@
-let width,
-  height,
-  ctx,
-  points,
-  target,
-  canvasTop,
-  animateHeader = true,
-  timer = null;
-const canvas = document.getElementById("js-canvas-hero");
-const container = document.querySelector(".grav-c-hero");
+import * as utils from "./utils.js";
+import Circle from "./Circle.js";
 
-function HeroAnimation() {
+let params = {
+  width: 0,
+  height: 0,
+  ctx: null,
+  points: [],
+  target: null,
+  canvasTop: null,
+  animateHeader: true,
+  canvas: null,
+  container: null
+};
+
+function HeroAnimation(canvas, container) {
+  if (typeof canvas.getContext !== "function") {
+    console.warn("the thingy bob doesn't support canvas. Bailing out.");
+    return;
+  }
+
   if (container !== null) {
-    canvasTop = canvas.getBoundingClientRect().top;
+    params.canvas = canvas;
+    params.container = container;
+    params.canvasTop = params.canvas.getBoundingClientRect().top;
+
     initHeader();
     initAnimation();
     addListeners();
   }
 }
 
-function resetTimer() {
-  window.clearTimeout(timer);
-  timer = null;
-  timer = window.setTimeout(initHeader, 60000);
-}
-
 function scrollCheck() {
-  if (document.body.scrollTop > height) {
-    animateHeader = false;
+  if (document.body.scrollTop > params.height) {
+    params.animateHeader = false;
   } else {
-    animateHeader = true;
+    params.animateHeader = true;
   }
 }
 
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
-
-const resizeCanvas = debounce(function() {
-  initHeader();
-  canvasTop = canvas.getBoundingClientRect().top;
+const resizeCanvas = utils.debounce(function() {
+  initHeader(params.canvas, params.container);
+  params.canvasTop = params.canvas.getBoundingClientRect().top;
 }, 150);
 
-function drawLines(p) {
-  if (!p.active) return;
-  for (var i in p.closest) {
-    ctx.beginPath();
-    ctx.moveTo(p.x, p.y);
-    ctx.lineTo(p.closest[i].x, p.closest[i].y);
-    ctx.strokeStyle = "rgba(94, 161, 184," + p.active + ")";
-    ctx.stroke();
-  }
-}
-
-function Circle(pos, rad, color) {
-  var _this = this;
-
-  // constructor
-  (function() {
-    _this.pos = pos || null;
-    _this.radius = rad || null;
-    _this.color = color || null;
-  })();
-
-  this.draw = function() {
-    if (!_this.active) return;
-    ctx.beginPath();
-    ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-    ctx.fillStyle = "rgba(94, 161, 184," + _this.active + ")";
-    ctx.fill();
-  };
-}
-
 function animate() {
-  if (animateHeader) {
-    ctx.clearRect(0, 0, width, height);
-    for (var i in points) {
-      // detect points in range
-      if (Math.abs(getDistance(target, points[i])) < 8000) {
-        points[i].active = 0.3;
-        points[i].circle.active = 0.75;
-      } else if (Math.abs(getDistance(target, points[i])) < 40000) {
-        points[i].active = 0.1;
-        points[i].circle.active = 0.3;
-      } else if (Math.abs(getDistance(target, points[i])) < 80000) {
-        points[i].active = 0.04;
-        points[i].circle.active = 0.1;
+  if (params.animateHeader) {
+    params.ctx.clearRect(0, 0, params.width, params.height);
+    params.points.map(point => {
+      if (Math.abs(utils.getDistance(params.target, point)) < 8000) {
+        point.active = 0.3;
+        point.circle.active = 0.75;
+      } else if (Math.abs(utils.getDistance(params.target, point)) < 40000) {
+        point.active = 0.1;
+        point.circle.active = 0.3;
+      } else if (Math.abs(utils.getDistance(params.target, point)) < 80000) {
+        point.active = 0.04;
+        point.circle.active = 0.1;
       } else {
-        points[i].active = 0.06;
-        points[i].circle.active = 0.06;
+        point.active = 0.04;
+        point.circle.active = 0.04;
       }
 
-      drawLines(points[i]);
-      points[i].circle.draw();
-    }
+      utils.drawLines(point, params.ctx);
+      point.circle.draw();
+    });
   }
+
   requestAnimationFrame(animate);
 }
 
-function shiftPoint(p) {
-  TweenLite.to(p, 1 + 1 * Math.random(), {
-    x: p.originX - 0 + Math.random() * 20,
-    y: p.originY - 0 + Math.random() * 20,
-    ease: Circ.easeInOut,
-    onComplete: function() {
-      shiftPoint(p);
-    }
-  });
+function mouseOut() {
+  params.target.x = params.container.clientHeight * 2;
+  params.target.y = params.container.clientHeight * 2;
 }
 
 function mouseMove(e) {
@@ -124,103 +79,71 @@ function mouseMove(e) {
   let posx = (posy = 0);
 
   posx = e.pageX;
-  posy = e.pageY - canvasTop;
+  posy = e.pageY - params.canvasTop;
 
-  target.x = posx;
-  target.y = posy;
+  params.target.x = posx;
+  params.target.y = posy;
 }
 
-function mouseEnter() {
-  initAnimation();
-}
+function noTouchAnim() {
+  let posy = 0;
+  let posx = (posy = 0);
 
-function mouseOut() {
-  target.x = container.clientHeight * 2;
-  target.y = container.clientHeight * 2;
-  resetTimer();
-}
+  posx = params.container.clientWidth / 2;
+  posy = params.container.clientHeight / 2 - params.canvasTop;
 
-function getDistance(p1, p2) {
-  return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+  params.target.x = posx;
+  params.target.y = posy;
 }
 
 function addListeners() {
-  let timer = null;
-
   if (!("ontouchstart" in window)) {
-    container.addEventListener("mousemove", mouseMove);
-    container.addEventListener("mouseout", mouseOut);
-    container.addEventListener("mouseenter", mouseEnter);
+    params.container.addEventListener("mousemove", mouseMove);
+    params.container.addEventListener("mouseout", mouseOut);
     window.addEventListener("resize", resizeCanvas);
+  } else {
+    noTouchAnim();
   }
 
   window.addEventListener("scroll", scrollCheck);
-
-  resetTimer();
 }
 
 function initHeader() {
-  width = container.clientWidth;
-  height = container.clientHeight;
-  canvas.width = width;
-  canvas.height = height;
-  target = {
-    x: width * 2,
-    y: height * 2
+  params.points = [];
+  params.width = params.container.clientWidth;
+  params.height = params.container.clientHeight;
+  params.canvas.width = params.width;
+  params.canvas.height = params.height;
+  params.target = {
+    x: params.width * 2,
+    y: params.height * 2
   };
 
-  if (typeof canvas.getContext !== "function") {
-    console.warn("the thingy bob doesn't support canvas. Bailing out.");
-    return;
-  }
+  params.ctx = params.canvas.getContext("2d");
 
-  ctx = canvas.getContext("2d");
-  target = {
-    x: width * 2,
-    y: height * 2
-  };
+  const pointsLimiter = utils.calcPointsLimiter(params.width, params.height);
 
-  // create points - added breakpoint with less nodes displayed as on smaller screens as it can look very dense
-  if (window.matchMedia("screen and (min-width:768px)").matches) {
-    points = [];
-    for (var x = 0; x < width; x = x + width / 9) {
-      for (var y = 0; y < height; y = y + height / 9) {
-        var px = x + Math.random() * width / 9;
-        var py = y + Math.random() * height / 9;
-        var p = {
-          x: px,
-          originX: px,
-          y: py,
-          originY: py
-        };
-        points.push(p);
-      }
-    }
-  } else {
-    points = [];
-    for (var x = 0; x < width; x = x + width / 6) {
-      for (var y = 0; y < height; y = y + height / 6) {
-        var px = x + Math.random() * width / 6;
-        var py = y + Math.random() * height / 6;
-        var p = {
-          x: px,
-          originX: px,
-          y: py,
-          originY: py
-        };
-        points.push(p);
-      }
+  for (var x = 0; x < params.width; x = x + params.width / pointsLimiter) {
+    for (var y = 0; y < params.height; y = y + params.height / pointsLimiter) {
+      var px = x + Math.random() * params.width / pointsLimiter;
+      var py = y + Math.random() * params.height / pointsLimiter;
+      var p = {
+        x: px,
+        originX: px,
+        y: py,
+        originY: py
+      };
+      params.points.push(p);
     }
   }
 
   // for each point find the 5 closest points
-  for (var i = 0; i < points.length; i++) {
-    var closest = [];
-    var p1 = points[i];
-    for (var j = 0; j < points.length; j++) {
-      var p2 = points[j];
+  params.points.map(p1 => {
+    let closest = [];
+    params.points.map(p2 => {
       if (!(p1 == p2)) {
         var placed = false;
+
         for (var k = 0; k < 5; k++) {
           if (!placed) {
             if (closest[k] == undefined) {
@@ -232,36 +155,35 @@ function initHeader() {
 
         for (var k = 0; k < 5; k++) {
           if (!placed) {
-            if (getDistance(p1, p2) < getDistance(p1, closest[k])) {
+            if (utils.getDistance(p1, p2) < utils.getDistance(p1, closest[k])) {
               closest[k] = p2;
               placed = true;
             }
           }
         }
       }
-    }
-    p1.closest = closest;
-  }
+    });
 
-  // assign a circle to each point
-  for (var i in points) {
-    var c = new Circle(
-      points[i],
+    p1.closest = closest;
+  });
+
+  params.points.map(point => {
+    point.circle = new Circle(
+      point,
       2 + Math.random() * 2.5,
-      "rgba(255,255,255,0.3)"
+      "rgba(94, 161, 184, 0.3)",
+      params.ctx
     );
-    points[i].circle = c;
-  }
+  });
 
   initAnimation();
 }
 
 function initAnimation() {
   animate();
-
-  for (var i in points) {
-    shiftPoint(points[i]);
-  }
+  params.points.map(point => {
+    utils.shiftPoint(point);
+  });
 }
 
 export default HeroAnimation;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,18 @@
 import Scroll from "./smoothScroll/scrollModule.js";
 import ScrollReveal from "./scrollReveal/scrollRevealModule.js";
 import toogleSwitch from "./toogleSwitch/toogleSwitch.js";
-import HeroAnimation from './heroAnimation/hero.js';
+import HeroAnimation from "./heroAnimation/hero.js";
+
+const canvas = document.getElementById("js-canvas-hero");
+const container = document.querySelector(".grav-c-hero");
 
 const scroll = new Scroll({ class: ".grav-c-cta" });
+
 const scrollReveal = new ScrollReveal();
-const heroAnimation = new HeroAnimation();
+
+if (canvas && container) {
+  const heroAnimation = new HeroAnimation(canvas, container);
+}
 
 scroll.init();
 scrollReveal.init();
