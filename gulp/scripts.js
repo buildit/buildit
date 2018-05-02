081eca16fe21380cf8be5426e1b38bbf98e133e0
Valerio Francescangeli <franz899@gmail.com>
2018-05-02 12:05:15 +0100

    New `dev` script that skips optimisation tasks

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -5,6 +5,9 @@ const rollup = require("rollup");
 const babel = require("rollup-plugin-babel");
 const closure = require("rollup-plugin-closure-compiler-js");
 const paths = require("../config.json").paths;
+const envs = require("./envs");
+
+const optimise = envs.shouldOptimise();
 
 function bundle() {
   return rollup
@@ -12,19 +15,22 @@ function bundle() {
       input: paths.scripts.main,
       plugins: [
         babel(),
-        closure({
-          compilationLevel: "SIMPLE",
-          // ES6 transpilation is made from babel
-          languageIn: "ECMASCRIPT5_STRICT",
-          languageOut: "ECMASCRIPT5_STRICT",
-          env: "CUSTOM",
-          warningLevel: "QUIET",
-          applyInputSourceMaps: false,
-          useTypesForOptimization: false,
-          processCommonJsModules: false,
-          // babel takes care of this
-          rewritePolyfills: false
-        })
+        gulpIf(
+          optimise,
+          closure({
+            compilationLevel: "SIMPLE",
+            // ES6 transpilation is made from babel
+            languageIn: "ECMASCRIPT5_STRICT",
+            languageOut: "ECMASCRIPT5_STRICT",
+            env: "CUSTOM",
+            warningLevel: "QUIET",
+            applyInputSourceMaps: false,
+            useTypesForOptimization: false,
+            processCommonJsModules: false,
+            // babel takes care of this
+            rewritePolyfills: false
+          })
+        )
       ]
     })
     .then(bundle => {
