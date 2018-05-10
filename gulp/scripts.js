3ca09ebe6966402e9b7cb551b2ded7773f105ec2
James Nash <james.nash@wipro.com>
2018-05-10 17:41:55 +0100

    Fixed bug where build info was stripped off optimised JS builds.

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -11,39 +11,46 @@ const getBuildInfo = require("./get-build-info.js");
 const optimise = envs.shouldOptimise();
 
 function bundle() {
-  return rollup
-    .rollup({
-      input: paths.scripts.main,
-      plugins: [
-        babel(),
-        gulpIf(
-          optimise,
-          closure({
-            compilationLevel: "SIMPLE",
-            // ES6 transpilation is made from babel
-            languageIn: "ECMASCRIPT5_STRICT",
-            languageOut: "ECMASCRIPT5_STRICT",
-            env: "CUSTOM",
-            warningLevel: "QUIET",
-            applyInputSourceMaps: false,
-            useTypesForOptimization: false,
-            processCommonJsModules: false,
-            // babel takes care of this
-            rewritePolyfills: false
-          })
-        )
-      ]
-    })
-    .then(bundle => {
-      return bundle.write({
-        file: path.join(paths.scripts.dest, "bundle.min.js"),
-        format: "umd",
-        sourcemap: true,
-        banner: getBuildInfo().then(
-          bldInfo => `/* ${bldInfo.description} ${bldInfo.commitShortHash} */`
-        )
+  return getBuildInfo().then(bldInfo => {
+    rollup
+      .rollup({
+        input: paths.scripts.main,
+        plugins: [
+          babel(),
+          gulpIf(
+            optimise,
+            closure({
+              compilationLevel: "SIMPLE",
+              // ES6 transpilation is made from babel
+              languageIn: "ECMASCRIPT5_STRICT",
+              languageOut: "ECMASCRIPT5_STRICT",
+              env: "CUSTOM",
+              warningLevel: "QUIET",
+              applyInputSourceMaps: false,
+              useTypesForOptimization: false,
+              processCommonJsModules: false,
+              // babel takes care of this
+              rewritePolyfills: false,
+              // Use Closure's outputWrapper to prepend build info
+              // for optimised builds
+              outputWrapper: `/* ${bldInfo.description} ${
+                bldInfo.commitShortHash
+              } */\n%output%`
+            })
+          )
+        ]
+      })
+      .then(bundle => {
+        return bundle.write({
+          file: path.join(paths.scripts.dest, "bundle.min.js"),
+          format: "umd",
+          sourcemap: true,
+          // For non-optimised builds
+          // (this gets stripped off by closure otheriwse)
+          banner: `/* ${bldInfo.description} ${bldInfo.commitShortHash} */`
+        });
       });
-    });
+  });
 }
 
 module.exports = {
