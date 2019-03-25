d09479182a51f3db2cf8d2182556843253c06c51
Daniel Wilkinson <daniel.wilkinson@wipro.com>
2019-03-25 16:53:58 +0000

    fix(test and locations): fixed tests and print locations on home page

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -8,4 +8,12 @@ describe("Homepage", () => {
   it("should render the page title", () => {
     cy.title().should("include", "Buildit @ Wipro Digital");
   });
+
+  it("should add a link for each location", () => {
+    cy.get(".colour-palette-dark")
+      .find("li")
+      .each(value => {
+        expect(value.find("a").attr("href")).to.include("/locations/");
+      });
+  });
 });
