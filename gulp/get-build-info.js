8c38dfb50b9b14d4593ef764c190f5604639250a
Daniel Wilkinson <daniel.wilkinson@wipro.com>
2019-03-26 11:01:49 +0000

    feat(humans.txt): updated humans.txt to a nunjucks template

--- gulp/get-build-info.js
+++ gulp/get-build-info.js
@@ -1,6 +1,7 @@
 const git = require("git-last-commit");
 const { promisify } = require("util");
 const envs = require("./envs.js");
+const moment = require("moment");
 
 // Holds cached commit info, so we don't need to
 // re-read it every time.
@@ -40,6 +41,9 @@ module.exports = async function() {
   bldInfo.commitGithubUrl = `https://github.com/buildit/gravity-metalsmith-shell/commit/${
     commitInfo.hash
   }`;
+  bldInfo.committedOn = moment
+    .unix(commitInfo.committedOn)
+    .format("YYYY-MM-DD HH:mm:ss");
 
   return bldInfo;
 };
