6206cc1ec42470f72612420cab50266830b2c06c
James Nash <james.nash@wipro.com>
2019-05-22 16:47:42 +0100

    refactor: removes flourishes code
    BREAKING CHANGE: The flourishes are no longer available for pages to use.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,8 @@
 import Scroll from "./smoothScroll/scrollModule.js";
-import ScrollReveal from "./scrollReveal/scrollRevealModule.js";
 import toogleSwitch from "./toogleSwitch/toogleSwitch.js";
 import HeroAnimation from "./heroAnimation/hero.js";
 
 const scroll = new Scroll({ class: ".grav-c-cta" });
-const scrollReveal = new ScrollReveal();
 const canvas = document.getElementById("js-canvas-hero");
 const container = document.querySelector(".grav-c-hero");
 
@@ -26,5 +24,4 @@ if (canvas && container) {
 }
 
 scroll.init();
-scrollReveal.init();
 toogleSwitch();
